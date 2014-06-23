2ecac0fcb2e86e912c0b0c32e9e96c8c13156d4d
Daniel Varga <dvarga@skawa.hu>
2014-06-23 14:24:21 +0200

    Added some extra inline comments to help with debugging further

--- iframe.dart
+++ iframe.dart
@@ -4,8 +4,9 @@ void main() {
 
   IFrameElement iframe = querySelector("#testframe");
   WindowBase win = iframe.contentWindow;
-  iframe.src = "./embeddedframe.html";
 
+  // The communication will be started from the embedded frame
+  // We will attempt to respond to the "READY" message by accessing the iframe's window object in different ways
   window.onMessage.listen((MessageEvent ev){
     print("PARENT RECEIVED DATA: ${ev.data}");
     win.postMessage("Using previously stored `win` variable to talk back", "*");
@@ -13,4 +14,5 @@ void main() {
     (ev.source as WindowBase).postMessage("Using event.source to talk back", "*");
   });
 
+  iframe.src = "./embeddedframe.html";
 }
